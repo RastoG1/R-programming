7cee5b1a7d3dc201809b1bc23ec69a0348343f52
RasťoG <88428745+RastoG1@users.noreply.github.com>
2022-11-18 16:48:39 +0100

    Update main.R

--- Projects/04. model building & residual analysis/main.R
+++ Projects/04. model building & residual analysis/main.R
@@ -4,7 +4,7 @@ install.packages("readxl")
 library(readxl)
 library(ggplot2)
 
-file = as.data.frame(read_excel('co2.xlsx'))
+file <- as.data.frame(read_excel('co2.xlsx'))
 head(file)
 
 ggplot(file, aes(x = c(1:132), y = co2)) + geom_line()
@@ -14,19 +14,19 @@ library(tseries)
 install.packages('forecast')
 library('forecast')
 
-x = ts(file$co2, frequency = 12)
-fit = tslm(x ~ trend  + season) 
+x <- ts(file$co2, frequency = 12)
+fit <- tslm(x ~ trend  + season) 
 fit
 summary(fit)
-x = as.numeric(x)
+x <- as.numeric(x)
 #plot(x, type = 'l')
-xx = ts(c(1:52), frequency = 4)
-y = predict(fit, xx)
+xx <- ts(c(1:52), frequency = 4)
+y <- predict(fit, xx)
 #plot(y, type = 'l')
 #lines(y, type = 'l', col = 'blue')
 
 
-z = file
+z <- file
 ggplot(file, aes(x = 1:length(co2))) + geom_line(aes(y = x, colour = 'y')) + geom_line(aes(y = y, colour = 'y^'))+
   scale_colour_manual("", 
                       breaks = c("y", "y^"),
