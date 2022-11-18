3c5b5a6bdcc120714039fa563e1ecb74698f85fa
RasťoG <88428745+RastoG1@users.noreply.github.com>
2022-11-18 16:51:25 +0100

    Update main.R

--- Projects/05. Moneyball/main.R
+++ Projects/05. Moneyball/main.R
@@ -8,7 +8,7 @@ summary(batting)
 any(is.na(batting))
 
 # On Base Percentage
-batting$OBP = (batting$H + batting$BB + batting$HBP)/(batting$AB + batting$BB + batting$HBP + batting$SF)
+batting$OBP <- (batting$H + batting$BB + batting$HBP)/(batting$AB + batting$BB + batting$HBP + batting$SF)
 
 # Creating X1B (Singles)
 batting$X1B <- batting$H - batting$X2B - batting$X3B - batting$HR
@@ -16,7 +16,7 @@ batting$X1B <- batting$H - batting$X2B - batting$X3B - batting$HR
 # Creating Slugging Average (SLG)
 batting$SLG <- ((1 * batting$X1B) + (2 * batting$X2B) + (3 * batting$X3B) + (4 * batting$HR) ) / batting$AB
 
-sal = read.csv('Salaries.csv')
+sal <- read.csv('Salaries.csv')
 
 subset(batting,yearID >= 1985)
 #or
@@ -26,11 +26,11 @@ library(dplyr)
 filter(batting, yearID >= 1985)
 
 #batting & sal data into one by playerID & yearID
-combo = merge(batting, sal, by = c('playerID', 'yearID'))
+combo <- merge(batting, sal, by = c('playerID', 'yearID'))
 
 summary(combo)
 
-lost_players = subset(combo,playerID %in% c('giambja01','damonjo01','saenzol01') )
+lost_players <- subset(combo,playerID %in% c('giambja01','damonjo01','saenzol01') )
 head(lost_players, 3)
 #or
 head(combo[combo$playerID == 'giambja01' | combo$playerID == 'damonjo01' | combo$playerID == 'saenzol01', ], 3)
@@ -39,16 +39,16 @@ head(combo[combo$playerID == 'giambja01' | combo$playerID == 'damonjo01' | combo
 #------------------------------------------------------------------
 
 #Saving money
-players = filter(combo, yearID == 2001)
+players <- filter(combo, yearID == 2001)
 #ggplot(players,aes(x=OBP,y=salary)) + geom_point()
 plot(players$OBP, players$salary)
 
-players = filter(players,salary < 8000000, OBP > 0)
-players = filter(players,AB >= 500)
+players <- filter(players,salary < 8000000, OBP > 0)
+players <- filter(players,AB >= 500)
 plot(players$OBP, players$salary)
 
-possible = head(arrange(players,desc(OBP)),10)
-possible = possible[,c('playerID','OBP','AB','salary')]
+possible <- head(arrange(players,desc(OBP)),10)
+possible <- possible[,c('playerID','OBP','AB','salary')]
 possible
 
 
